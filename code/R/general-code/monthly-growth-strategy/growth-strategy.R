83870a791b6794638e918d62ba36561e8e38f164
Alessandro Ferrari <alessandro.ferrari.2004@gmail.com>
2021-07-04 22:48:51 -0400

    moar code

--- code/R/general-code/monthly-growth-strategy/growth-strategy.R
+++ code/R/general-code/monthly-growth-strategy/growth-strategy.R
@@ -6,14 +6,22 @@ library(dplyr)
 library(reshape2)
 library(plotly)
 library(dygraphs)
+library(xts)
+
+########### VARIABLES ########################
+nStocks <- 4 # stocks to pick each time frame
+analysisTimeFrame <- 20 # 1 month in trading days
+startYear <- "2015"
+startMonth <- "01"
+startDay <- "01"
 
 ########### CHOOSE STOCKS AND TIME FRAME ######
-stockArray <- c("AAPL", "MSFT", "RTX", "GOOGL", "V", "AMZN", "NVDA", "KO")
-startDate <- paste0("2010-", "01-", "01")
+stockArray <- c("AAPL", "MSFT", "RTX", "GOOGL", "V", "AMZN", "NVDA", "KO", "STT", "ROK", "BA", "GWW", "TJX", "WBA", "MCD", "QCOM", "AMD")
+startDate <- paste0(startYear, "-", startMonth, "-", startDay)
 
 ########### GET STOCK DATA ####################
 stock <- lapply(as.list(stockArray), function(x) {
-    tmp <- try(getSymbols(x, from=startDate,   auto.assign= FALSE))
+    tmp <- try(getSymbols(x, from=startDate,   auto.assign = FALSE))
     if(!inherits(tmp, 'try-error'))
         tmp})
 
@@ -22,13 +30,26 @@ allData <- as.data.frame(stock)
 
 closeData <- select(allData, contains("Close"))
 
+rm(allData)
+
 closeDataTime <- as.xts(closeData)
 
 returnsList <- apply(closeDataTime, 2, quantmod::Delt) + 1
 returnsList = returnsList[-1,]
 
+applied <- apply(returnsList, 2, cumprod)
 rownames(applied) = rownames(closeData)[-1]
 
-applied <- apply(returnsList, 2, cumprod)
+applied <- as.xts(applied)
 
-dygraph(applied)
+dygraph(applied,
+        main = paste0("Returns for ", paste(stockArray, collapse = ', '))
+        ) %>%
+    dyLegend(show = "follow") %>%
+    dyEvent("2014-01-01", "WSB", labelLoc = "bottom")
+
+investment <- data.frame(row.names = rownames(applied))
+
+for (i in 1:nrow(applied)) {
+    
+}
